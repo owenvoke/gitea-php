a2c0b7e85509512a40885be6dbf930e204f32e41
Owen Voke <development@voke.dev>
2020-09-30 09:40:56 +0100

    tests(organization): add tests

---
+++ tests/Api/OrganizationTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+use OwenVoke\Gitea\Api\Organization;
+
+beforeEach(fn () => $this->apiClass = Organization::class);
+
+it('should get all organisations', function () {
+    $expectedArray = [['id' => 1, 'username' => 'TestOrg']];
+
+    $api = $this->getApiMock();
+
+    $api->expects($this->once())
+        ->method('get')
+        ->with('/orgs')
+        ->will($this->returnValue($expectedArray));
+
+    expect($api->all())->toBe($expectedArray);
+});
+
+it('should show an organisation', function () {
+    $expectedArray = ['id' => 1, 'username' => 'TestOrg'];
+
+    $api = $this->getApiMock();
+
+    $api->expects($this->once())
+        ->method('get')
+        ->with('/orgs/TestOrg')
+        ->will($this->returnValue($expectedArray));
+
+    expect($api->show('TestOrg'))->toBe($expectedArray);
+});
+
+it('should update an organisation', function () {
+    $expectedArray = ['id' => 1, 'username' => 'TestOrg'];
+
+    $api = $this->getApiMock();
+
+    $api->expects($this->once())
+        ->method('patch')
+        ->with('/orgs/TestOrg', ['value' => 'toUpdate'])
+        ->will($this->returnValue($expectedArray));
+
+    expect($api->update('TestOrg', ['value' => 'toUpdate']))->toBe($expectedArray);
+});
+
+it('should get an organisations repositories', function () {
+    $expectedArray = [['id' => 1, 'name' => 'gitea-php']];
+
+    $api = $this->getApiMock();
+
+    $api->expects($this->once())
+        ->method('get')
+        ->with('/orgs/TestOrg/repos')
+        ->will($this->returnValue($expectedArray));
+
+    expect($api->repositories('TestOrg'))->toBe($expectedArray);
+});
