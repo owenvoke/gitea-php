62e1a57daac87afce3ce8b4e27016d244d6ea922
Owen Voke <development@voke.dev>
2020-11-18 09:17:25 +0000

    tests: add test to retrieve instances

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,7 +2,34 @@
 
 declare(strict_types=1);
 
+use OwenVoke\Gitea\Api\CurrentUser;
+use OwenVoke\Gitea\Api\Organization;
+use OwenVoke\Gitea\Api\Repo;
+use OwenVoke\Gitea\Api\User;
 use OwenVoke\Gitea\Client;
+
+it('gets instances from the client', function () {
+    $client = new Client();
+
+    // Retrieves CurrentUser instance
+    expect($client->currentUser())->toBeInstanceOf(CurrentUser::class);
+    expect($client->current_user())->toBeInstanceOf(CurrentUser::class);
+    expect($client->me())->toBeInstanceOf(CurrentUser::class);
+
+    // Retrieves Organization instance
+    expect($client->organization())->toBeInstanceOf(Organization::class);
+    expect($client->organizations())->toBeInstanceOf(Organization::class);
+
+    // Retrieves User instance
+    expect($client->repo())->toBeInstanceOf(Repo::class);
+    expect($client->repos())->toBeInstanceOf(Repo::class);
+    expect($client->repository())->toBeInstanceOf(Repo::class);
+    expect($client->repositories())->toBeInstanceOf(Repo::class);
+
+    // Retrieves User instance
+    expect($client->user())->toBeInstanceOf(User::class);
+    expect($client->users())->toBeInstanceOf(User::class);
+});
 
 it('allows setting a custom url', function () {
     $client = new Client(null, null, 'https://gitea.test');
