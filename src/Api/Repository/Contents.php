4afd139034ca0174fc23063c493bbdce719a1a96
Your Name <rosven9856@gmail.com>
2025-05-01 20:19:50 +0300

    feature (repository contents): updating a file in the repository

--- src/Api/Repository/Contents.php
+++ src/Api/Repository/Contents.php
@@ -17,7 +17,7 @@ class Contents extends AbstractApi
         return $this->get(sprintf($format, rawurlencode($owner), rawurlencode($repository), rawurlencode($ref)));
     }
 
-    public function create(string $owner, string $repository, array $parameters): array
+    public function bulkModify(string $owner, string $repository, array $parameters): array
     {
         if (! isset($parameters['files'])) {
             throw new MissingArgumentException(['files']);
@@ -26,7 +26,7 @@ class Contents extends AbstractApi
         return $this->post(sprintf('/repos/%s/%s/contents', rawurlencode($owner), rawurlencode($repository)), $parameters);
     }
 
-    public function getFile(string $owner, string $repository, string $filepath, string $ref = ''): array
+    public function show(string $owner, string $repository, string $filepath, string $ref = ''): array
     {
         $format = '/repos/%s/%s/contents/%s';
         if (!empty($ref)) {
@@ -35,4 +35,21 @@ class Contents extends AbstractApi
 
         return $this->get(sprintf($format, rawurlencode($owner), rawurlencode($repository), rawurlencode($filepath), rawurlencode($ref)));
     }
+
+    public function update(string $owner, string $repository, string $filepath, array $params)
+    {
+        if (!isset($params['content'])) {
+            throw new MissingArgumentException(['content']);
+        }
+
+        if (!isset($params['sha'])) {
+            throw new MissingArgumentException(['sha']);
+        }
+
+        return $this->put(sprintf('/repos/%s/%s/contents/%s', rawurlencode($owner), rawurlencode($repository), rawurlencode($filepath)), $params);
+    }
+
+    // create
+
+    // remove
 }
--- tests/Api/Repository/ContentsTest.php
+++ tests/Api/Repository/ContentsTest.php
@@ -42,14 +42,14 @@ it('should get files of the root dir for a repository by the ref', function () {
 });
 
 it('should modify files in repository without using the files option', function () {
-    $data = ['message' => 'modify files'];
+    $data = [];
 
     $api = $this->getApiMock();
     $api->expects($this->never())
         ->method('post')
         ->with('/repos/owenvoke/gitea-php/contents', $data);
 
-    $api->create('owenvoke', 'gitea-php', $data);
+    $api->bulkModify('owenvoke', 'gitea-php', $data);
 })->throws(MissingArgumentException::class);
 
 it('should modify files in repository using option files', function () {
@@ -62,7 +62,7 @@ it('should modify files in repository using option files', function () {
         ->with('/repos/owenvoke/gitea-php/contents', $data)
         ->willReturn($expectedValue);
 
-    expect($api->create('owenvoke', 'gitea-php', $data))->toBe($expectedValue);
+    expect($api->bulkModify('owenvoke', 'gitea-php', $data))->toBe($expectedValue);
 });
 
 it('should get file for a repository', function () {
@@ -78,7 +78,7 @@ it('should get file for a repository', function () {
         ->with('/repos/owenvoke/gitea-php/contents/content.txt')
         ->willReturn($expectedValue);
 
-    expect($api->getFile('owenvoke', 'gitea-php', 'content.txt'))->toBe($expectedValue);
+    expect($api->show('owenvoke', 'gitea-php', 'content.txt'))->toBe($expectedValue);
 });
 
 it('should get file for a repository by the ref', function () {
@@ -94,5 +94,47 @@ it('should get file for a repository by the ref', function () {
         ->with('/repos/owenvoke/gitea-php/contents/content.txt?ref=main')
         ->willReturn($expectedValue);
 
-    expect($api->getFile('owenvoke', 'gitea-php', 'content.txt', 'main'))->toBe($expectedValue);
+    expect($api->show('owenvoke', 'gitea-php', 'content.txt', 'main'))->toBe($expectedValue);
+});
+
+it('should update a file in the repository without using the content option', function () {
+    $data = [];
+
+    $api = $this->getApiMock();
+    $api->expects($this->never())
+        ->method('put')
+        ->with('/repos/owenvoke/gitea-php/contents/content.txt', $data);
+
+    $api->update('owenvoke', 'gitea-php', 'content.txt', $data);
+})->throws(MissingArgumentException::class);
+
+it('should update a file in the repository without using the sha option', function () {
+    $data = ['content' => ''];
+
+    $api = $this->getApiMock();
+    $api->expects($this->never())
+        ->method('put')
+        ->with('/repos/owenvoke/gitea-php/contents/content.txt', $data);
+
+    $api->update('owenvoke', 'gitea-php', 'content.txt', $data);
+})->throws(MissingArgumentException::class);
+
+it('should update a file in the repository', function () {
+    $expectedValue = [
+        'content' => [
+            '_links' => [], 'content' => '', 'download_url' => '', 'encoding' => '', 'git_url' => '', 'html_url' => '',
+            'last_commit_sha' => '', 'name' => '', 'path' => '', 'sha' => '', 'size' => 0, 'submodule_git_url' => '',
+            'target' => '', 'type' => '', 'url' => ''
+        ]
+    ];
+
+    $data = ['content' => '', 'sha' => ''];
+
+    $api = $this->getApiMock();
+    $api->expects($this->once())
+        ->method('put')
+        ->with('/repos/owenvoke/gitea-php/contents/content.txt')
+        ->willReturn($expectedValue);
+
+    expect($api->update('owenvoke', 'gitea-php', 'content.txt', $data))->toBe($expectedValue);
 });
